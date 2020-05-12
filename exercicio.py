85a91bc3b07785ac01fc3c559fd7a00d333e385c
ricardozv <ricardosilvanet03@gmail.com>
2020-05-12 01:16:29 -0300

    up

--- exercicio.py
+++ exercicio.py
@@ -3,4 +3,4 @@ d = n * 2
 t = n * 3
 r = n** (1/2)
 print ('O dobro de {} vale {}.' .format(n, d))
-print ('O triplo de {} vale {}. A raiz quadrada de {} é igual a {}.'.format(n, t,n, r)) 
+print ('O triplo de {} vale {}.\n A raiz quadrada de {} é igual a {:.2f}.'.format(n, t, n, r)) 
